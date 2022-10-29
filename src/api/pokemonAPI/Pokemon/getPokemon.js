2d65811c46d5069a7f0247605f45b058fd7e65ee
Mayko2003 <maykox34f@gmail.com>
2022-10-28 22:21:25 -0300

    fix: use GetEnvironments helper
    Use getEnvironments helper for use Environments Vars

--- src/api/pokemonAPI/Pokemon/getPokemon.js
+++ src/api/pokemonAPI/Pokemon/getPokemon.js
@@ -1,7 +1,8 @@
-
+import { getEnviroments } from "../../../helpers/getEnvironments";
 export const getPokemon = async (id = null, name = null) => {
+    const env = getEnviroments();
     const response = await fetch(
-        `${import.meta.env.VITE_POKEAPI_HOST}/pokemon/${id || name}`
+        `${env.VITE_POKEAPI_HOST}/pokemon/${id || name}`
     );
     const data = await response.json();
     return data;
