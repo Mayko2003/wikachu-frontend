b5f78b87a40fd2003d70caba9829c73c2e1c9270
Mayko2003 <maykox34f@gmail.com>
2022-12-26 02:39:14 -0300

    feat: add PokemonCard component, hardcoded

--- src/wikachu/components/PokemonCard.jsx
+++ src/wikachu/components/PokemonCard.jsx
@@ -1,9 +1,53 @@
+import { useLike } from '../hooks/useLike'
+import './styles.css'
+import { TypeTag } from './TypeTag'
+
+export const PokemonCard = ({ pokemon }) => {
+
+    const { onChangeLike } = useLike()
 
 
-export const PokemonCard = ({pokemon}) => {
+    const handlePokemonClick = (e) => {
+        e.target.classList.add('animate__animated', 'animate__tada')
+
+
+        setTimeout(() => {
+            e.target.classList.remove('animate__animated', 'animate__tada')
+        }, 1000)
+    }
+
+
+
     return (
-        <div>
-            
+        <div className='col-lg-3 col-md-4 p-1' >
+            <div className="card bg-transparent shadow h-100">
+
+                <img src="/icons/unliked.png" alt="" className='card-icon mx-auto mt-3' onClick={onChangeLike} />
+
+                <div className='mx-auto'>
+                    <img
+                        src={pokemon.image}
+                        alt={pokemon.name}
+                        className="card-img-top"
+                        onClick={handlePokemonClick}
+                    />
+                </div>
+                <div className="card-body d-flex flex-column">
+                    <h5 className='text-muted text-center'>#{pokemon.id}</h5>
+
+                    <a href="" className='nav-link link-secondary fs-2 p-0'>
+                        {pokemon.name}
+                    </a>
+
+                    <div className='d-flex mt-auto'>
+                        {
+                            pokemon.types.map(type => (
+                                <TypeTag key={type.name} type={type} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
