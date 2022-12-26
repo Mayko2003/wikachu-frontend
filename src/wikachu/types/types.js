9b18432554bfd3431bee18d6fa083eff7ed38883
Mayko2003 <maykox34f@gmail.com>
2022-12-26 04:54:02 -0300

    chore: add wikachu types

---
+++ src/wikachu/types/types.js
@@ -0,0 +1,11 @@
+export const types = {
+    like: {
+        pokemon: '[Like] Pokemon',
+        item: '[Like] Item',
+        move: '[Like] Move',
+        ability: '[Like] Ability',
+        location: '[Like] Location',
+        type: '[Like] Type',
+        nature: '[Like] Nature',
+    }
+}
