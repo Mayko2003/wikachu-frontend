5ce2ed5bf66d5708066c05e96ed61a0f49e8a8e7
Mayko2003 <maykox34f@gmail.com>
2022-12-26 04:56:32 -0300

    chore: use Pokemon hook for get data

--- src/wikachu/components/PokemonList.jsx
+++ src/wikachu/components/PokemonList.jsx
@@ -1,11 +1,16 @@
+import { usePokemon } from "../hooks/usePokemon"
 import { PokemonCard } from "./PokemonCard"
 
 
+export const PokemonList = ({amount}) => {
 
-export const PokemonList = ({ pokemons }) => {
+    const { pokemons } = usePokemon(amount);
+
+
     return (
         <div className="row mx-auto col-md-8 animate__animated animate__fadeIn">
             {
+                pokemons.length > 0 &&
                 pokemons.map(pokemon => (
                     <PokemonCard key={pokemon.id} pokemon={pokemon} />
                 ))
