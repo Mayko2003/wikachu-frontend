383387ab4e62bc35f569d31bbd6b696fa61f0ada
Mayko2003 <maykox34f@gmail.com>
2022-12-29 19:28:04 -0300

    chore: init like reducer

--- src/wikachu/context/likeReducer.js
+++ src/wikachu/context/likeReducer.js
@@ -1,23 +1,13 @@
 import { types } from "../types/types"
 
 
-export const likeReducer = (state = {}, action) => {
+export const likeReducer = (state = false, action) => {
 
     switch (action.type) {
-        case types.like.pokemon:
-            break;
-        case types.like.item:
-            break;
-        case types.like.move:
-            break;
-        case types.like.ability:
-            break;
-        case types.like.location:
-            break;
-        case types.like.type:
-            break;
-        case types.like.nature:
-            break;
+        case types.create:
+            return true;
+        case types.delete:
+            return false;
         default:
             return state;
     }
