798eb98f73ed46b98bf9216208a631cc6693605d
Mayko2003 <maykox34f@gmail.com>
2022-12-29 19:23:00 -0300

    chore: init private route hoc

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,9 +1,11 @@
+import { useContext } from "react"
+import { AuthContext } from "../auth/context"
+import { Navigate } from "react-router-dom"
+
+export const PrivateRoute = ({ children }) => {
+
+    const { logged } = useContext(AuthContext)
 
 
-export const PrivateRoute = ({children}) => {
-
-    
-    return (
-        <div>PrivateRoute</div>
-    )
+    return logged? children : <Navigate to="/login" />
 }
