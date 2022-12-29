a219991c41c35acdd7d41dce15ea552b8d6465f5
Mayko2003 <maykox34f@gmail.com>
2022-12-29 19:27:10 -0300

    chore: change likeReducer types

--- src/wikachu/types/types.js
+++ src/wikachu/types/types.js
@@ -1,11 +1,4 @@
 export const types = {
-    like: {
-        pokemon: '[Like] Pokemon',
-        item: '[Like] Item',
-        move: '[Like] Move',
-        ability: '[Like] Ability',
-        location: '[Like] Location',
-        type: '[Like] Type',
-        nature: '[Like] Nature',
-    }
+    create: 'CREATE',
+    delete: 'DELETE',
 }
