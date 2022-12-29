24cf3930144ab99f36078756bcb379040decd6eb
Mayko2003 <maykox34f@gmail.com>
2022-12-29 19:29:54 -0300

    chore: add token validation

--- src/api/backend/favourite/checkFavourite.js
+++ src/api/backend/favourite/checkFavourite.js
@@ -1,12 +1,14 @@
 import { getEnviroments } from "../../../helpers/getEnvironments";
 
-export const checkFavourite = async (user, pokemon) => {
+export const checkFavourite = async (user, pokemon, token) => {
     try {
+        if (!token) throw new Error('No token provided');
         const env = getEnviroments();
         const options = {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
             },
             body: JSON.stringify({
                 user,
@@ -16,7 +18,7 @@ export const checkFavourite = async (user, pokemon) => {
         const response = await fetch(
             `${env.VITE_BACKEND_URL}/favourite/check`, options
         );
-        const data = await response.json();
+        const {data} = await response.json();
         return data;
     }
     catch (error) {
