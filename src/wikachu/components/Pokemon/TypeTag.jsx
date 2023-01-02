6f9c76036ec7cbfbca1b49b307d7f7c414d1c08f
Mayko2003 <maykox34f@gmail.com>
2023-01-02 03:12:31 -0300

    style: add width to button

--- src/wikachu/components/Pokemon/TypeTag.jsx
+++ src/wikachu/components/Pokemon/TypeTag.jsx
@@ -2,7 +2,7 @@
 
 export const TypeTag = ({ type }) => {
     return (
-        <button className='btn text-light me-1 btn-sm' style={{ backgroundColor: type.color }}>
+        <button className='btn text-light me-1 mt-1 btn-sm' style={{ backgroundColor: type.color, width: '6em' }}>
             {type.name}
         </button>
     )
