f97dda349bb4076acbe4fff6fbe6a6a6dff8d540
Mayko2003 <maykox34f@gmail.com>
2023-01-01 22:27:23 -0300

    chore: add Loading component

---
+++ src/ui/components/Loading.jsx
@@ -0,0 +1,12 @@
+
+
+export const Loading = () => {
+    return (
+
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border text-warning" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
+}
