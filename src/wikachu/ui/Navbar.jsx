2a136f4f27e1577aecc7ca88e04227ae781b3a90
Mayko2003 <maykox34f@gmail.com>
2023-01-01 22:40:03 -0300

    chore: use url param for links redirects

--- src/wikachu/ui/Navbar.jsx
+++ src/wikachu/ui/Navbar.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const { id_name } = useParams();
+
     return (
-        <ul className="nav nav-tabs">
+        <ul className="nav nav-tabs mb-3">
             <NavLink
-                        className={({isActive})=>`nav-item nav-link ${ isActive ? 'active' : ''}`} 
-                        to="/"
+                className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                to={`/pokemon/${id_name}/`}
             >
                 Pokedex Data
             </NavLink>
             <NavLink
-                        className={({isActive})=>`nav-item nav-link ${ isActive ? 'active' : ''}`} 
-                        to="/stats"
+                className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                to={`/pokemon/${id_name}/stats`}
             >
                 Stats
             </NavLink>
             <NavLink
-                        className={({isActive})=>`nav-item nav-link ${ isActive ? 'active' : ''}`} 
-                        to="/evolutions"
+                className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                to={`/pokemon/${id_name}/evolutions`}
             >
                 Evolutions
             </NavLink>
             <NavLink
-                        className={({isActive})=>`nav-item nav-link ${ isActive ? 'active' : ''}`} 
-                        to="/moves"
+                className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                to={`/pokemon/${id_name}/moves`}
             >
                 Moves
             </NavLink>
