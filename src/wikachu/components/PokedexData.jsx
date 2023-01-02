1a03d0977fce9eba8c88c5edfca97af1f645a289
Mayko2003 <maykox34f@gmail.com>
2023-01-01 22:29:21 -0300

    style: add css classes and import styles

--- src/wikachu/components/PokedexData.jsx
+++ src/wikachu/components/PokedexData.jsx
@@ -1,34 +1,35 @@
 import { AbilitiesList, TypesList } from './';
+import './styles.css'
 
-export const PokedexData = ({pokemon}) => {
+export const PokedexData = ({ pokemon }) => {
 
-    const {id, height, weight, abilities, types} = pokemon;
+    const { id, height, weight, abilities, types } = pokemon;
 
     return (
-        <>
+        <div className='animate__animated animate__fadeInUp'>
             <h2>Pokedex Data</h2>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    <strong>Id: </strong>  
+            <ul className="list-group glass-card">
+                <li className="list-group-item bg-transparent text-white">
+                    <strong className='text-muted'>National N°: </strong>
                     {id}
                 </li>
-                <li className="list-group-item">
-                    <strong>Weight: </strong> 
-                    { weight/10 } kg
+                <li className="list-group-item bg-transparent text-white">
+                    <strong className='text-muted'>Weight: </strong>
+                    {weight / 10} kg
                 </li>
-                <li className="list-group-item">
-                    <strong>Height: </strong>
-                    {height/10 } m
+                <li className="list-group-item bg-transparent text-white">
+                    <strong className='text-muted'>Height: </strong>
+                    {height / 10} m
                 </li>
-                <li className="list-group-item">
-                    <h4>Abilities</h4>
-                    <AbilitiesList abilities={abilities}/>
+                <li className="list-group-item bg-transparent text-white">
+                    <h4 className='text-muted'>Abilities</h4>
+                    <AbilitiesList abilities={abilities} />
                 </li>
-                <li className="list-group-item">
-                    <h4>Types</h4>
-                    <TypesList types={types}/>
+                <li className="list-group-item bg-transparent text-white">
+                    <h4 className='text-muted mb-3'>Types</h4>
+                    <TypesList types={types} />
                 </li>
             </ul>
-        </>
+        </div>
     )
 }
