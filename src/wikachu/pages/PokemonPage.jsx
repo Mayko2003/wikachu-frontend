024cf3bc810953ac7719aa93bdf1d90e68878584
Mayko2003 <maykox34f@gmail.com>
2023-01-01 22:42:01 -0300

    chore: use url param, add sprites...
    use url param for get pokemon, structure sprites of pokemon, change PokemonInfoRouter by renamed component

--- src/wikachu/pages/PokemonPage.jsx
+++ src/wikachu/pages/PokemonPage.jsx
@@ -1,23 +1,47 @@
-import { BrowserRouter } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { capitalize } from "../helpers/capitalize";
 import { useGetPokemon } from "../hooks/useGetPokemon";
-import { PokemonInfoRouter } from "../router/PokemonInfoRouter";
+import { PokemonRoutes } from "../routes/PokemonRoutes";
 
-export const PokemonPage = ({ id = 1 }) => {
+export const PokemonPage = () => {
 
-    const { pokemon } = useGetPokemon(id);
+    const { id_name } = useParams();
 
-    const imagePokemon = pokemon['sprites']?.['other']['official-artwork']['front_default'];
+    const { pokemon } = useGetPokemon(id_name);
+
+    const sprites = {
+        home: {
+            frontDefault: pokemon['sprites']?.['other']['home']['front_default'],
+            frontShiny: pokemon['sprites']?.['other']['home']['front_shiny'],
+        },
+        official: pokemon['sprites']?.['other']['official-artwork']['front_default'],
+        dreamWorld: pokemon['sprites']?.['other']['dream_world']['front_default'],
+        default: {
+            male: {
+                frontDefault: pokemon['sprites']?.['front_default'],
+                frontShiny: pokemon['sprites']?.['front_shiny'],
+                backDefault: pokemon['sprites']?.['back_default'],
+                backShiny: pokemon['sprites']?.['back_shiny'],
+            },
+            female: {
+                frontDefault: pokemon['sprites']?.['front_female'],
+                frontShiny: pokemon['sprites']?.['front_shiny_female'],
+                backDefault: pokemon['sprites']?.['back_female'],
+                backShiny: pokemon['sprites']?.['back_shiny_female']
+            }
+        }
+    }
 
     return (
         <>
-            <h1 className="text-light">{pokemon.name}</h1>
+            <h1 className="text-light text-center my-5">{capitalize(pokemon.name)}</h1>
 
             <div className="row m-0">
-                <div className="col-12 col-md-4">
-                    <img src={imagePokemon} alt="" className="w-100" />
+                <div className="col-12 col-lg-4 d-flex justify-content-center animate__animated animate__fadeInLeft">
+                    <img src={sprites.home.frontDefault} alt="" className="img-front-pokemon img-fluid" />
                 </div>
-                <div className="col-12 col-md-8 text-light">
-                        <PokemonInfoRouter data={pokemon}/>
+                <div className="col-12 col-lg-8 text-light ">
+                    <PokemonRoutes data={pokemon} />
                 </div>
             </div>
         </>
