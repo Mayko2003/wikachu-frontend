9c9758ea9d3529710111d09fb8f4e6f85030b73e
Mayko2003 <maykox34f@gmail.com>
2023-01-02 04:44:09 -0300

    style: add bootstrap classes

--- src/wikachu/components/PokemonInfo/StatsInfo.jsx
+++ src/wikachu/components/PokemonInfo/StatsInfo.jsx
@@ -1,3 +1,4 @@
+import { capitalize, splitCharacter } from "../../../helpers";
 import { getMinMaxStats } from "../../helpers";
 
 export const StatsInfo = ({ stats }) => {
@@ -8,48 +9,67 @@ export const StatsInfo = ({ stats }) => {
         <div className="animate__animated animate__fadeInUp">
             <h2>Stats</h2>
             <div className="row glass-card m-0 p-2">
-                <div className="col-3">
+                <div className="col-xl-3 mb-xl-0 mb-2 d-flex flex-column">
                     <h2 className="h5">Base Stats</h2>
-                    <ul>
-                        {stats?.map(({ base_stat, stat }) => (
-                            <li key={stat.name}>{stat.name}: {base_stat}</li>
-                        ))}
+                    <ul className="list-group h-100">
+                        {
+                            stats?.map(({ base_stat, stat }) => (
+                                <li key={stat.name} className='list-group-item bg-transparent text-light'>{capitalize(splitCharacter(stat.name, '-'))}: {base_stat}</li>
+                            ))
+                        }
+                        <li className="list-group-item bg-transparent text-light">
+                            <strong>Total: </strong> {stats?.reduce((acc, { base_stat }) => acc + base_stat, 0) || 0}
+                        </li>
                     </ul>
                 </div>
-                <div className="col-5">
+                <div className="col-xl-5 mb-xl-0 mb-2 d-flex flex-column">
                     {/* Grafico */}
                     <h2 className="h5">Graphic</h2>
-                    {
-                        stats?.map(({ base_stat, stat }) =>
-                            <div className="progress mb-2 glass-card" key={stat.name}>
-                                <div
-                                    className='progress-bar bg-warning'
-                                    role="progressbar"
-                                    aria-valuenow="0"
-                                    aria-valuemin="0"
-                                    aria-valuemax="1000"
-                                    style={{ width: `${base_stat * 100 / 255}%`, opacity: 0.5  }}
-                                >
-                                </div>
-                            </div>
-                        )
-                    }
+                    <ul className="list-group h-100">
+                        {
+                            stats?.map(({ base_stat, stat }) =>
+                                <li className="list-group-item bg-transparent text-light h-100" key={stat.name}>
+                                    <div className="progress glass-card">
+                                        <div
+                                            className='progress-bar bg-warning'
+                                            role="progressbar"
+                                            aria-valuenow="0"
+                                            aria-valuemin="0"
+                                            aria-valuemax="1000"
+                                            style={{ width: `${base_stat * 100 / 255}%`, opacity: 0.5 }}
+                                        >
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        }
+                        <li className="list-group-item bg-transparent text-light h-100 border-0"></li>
+                    </ul>
+
                 </div>
-                <div className="col-2">
+                <div className="col-xl-2 col-6 d-flex flex-column">
                     <h2 className="h5">Min Stats</h2>
-                    <ul>
+                    <ul className="list-group h-100">
                         {minStats?.map(({ value, name }) => (
-                            <li key={name}>{value}</li>
+                            <li className="list-group-item bg-transparent text-light h-100" key={name}>{value}</li>
                         ))}
+                        <li className="list-group-item bg-transparent text-light h-100 border-0"></li>
                     </ul>
                 </div>
-                <div className="col-2">
+                <div className="col-xl-2 col-6 d-flex flex-column">
                     <h2 className="h5">Max Stats</h2>
-                    <ul>
+                    <ul className="list-group h-100">
                         {maxStats?.map(({ value, name }) => (
-                            <li key={name}>{value}</li>
+                            <li className="list-group-item bg-transparent text-light h-100" key={name}>{value}</li>
                         ))}
+                        <li className="list-group-item bg-transparent text-light h-100 border-0"></li>
                     </ul>
+                </div>
+
+                <div className="text-muted mt-3">
+                    <p>
+                        The ranges shown on min and max stats are for a level 100 Pokémon. Maximum values are based on a beneficial nature, 252 EVs, 31 IVs however minimum values are based on a hindering nature, 0 EVs, 0 IVs.
+                    </p>
                 </div>
             </div>
         </div>
