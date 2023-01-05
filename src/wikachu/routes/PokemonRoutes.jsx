41816232175312dc1ede2a5a317d9fa478275312
Mayko2003 <maykox34f@gmail.com>
2023-01-05 13:49:07 -0300

    chore: add training and breeding routes

--- src/wikachu/routes/PokemonRoutes.jsx
+++ src/wikachu/routes/PokemonRoutes.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import { EvolutionInfo, MovesInfo, PokedexData, StatsInfo, NavTabs } from '../components/PokemonInfo'
+import { TrainingInfo } from '../components/PokemonInfo/TrainingInfo'
+import { BreedingInfo } from '../components/PokemonInfo/BreedingInfo'
+import { getGender } from '../helpers'
 export const PokemonRoutes = ({ data }) => {
 
 
@@ -11,6 +14,8 @@ export const PokemonRoutes = ({ data }) => {
                 <Route path='stats' element={<StatsInfo stats={data.stats} />} />
                 <Route path='evolutions' element={<EvolutionInfo />} />
                 <Route path='moves' element={<MovesInfo moves={data.moves} />} />
+                <Route path='training' element={<TrainingInfo info={{ ...data.specie, base_exp: data.base_experience, stats: data.stats }} />} />
+                <Route path='breeding' element={<BreedingInfo info={{ ...data.specie, gender: getGender(data.specie.gender_rate) }} />} />
             </Routes>
         </>
     )
