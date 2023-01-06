0f4cfa21597daf569a8b5e6594d8edc7f30af589
Mayko2003 <maykox34f@gmail.com>
2023-01-05 23:35:48 -0300

    chore: add try-catch

--- src/api/pokemonAPI/Pokemon/getSpecie.js
+++ src/api/pokemonAPI/Pokemon/getSpecie.js
@@ -1,9 +1,14 @@
 import { getEnviroments } from "../../../helpers/getEnvironments";
 export const getSpecie = async (specie) => {
-    const env = getEnviroments();
-    const response = await fetch(
-        `${env.VITE_POKEAPI_HOST}/pokemon-species/${specie}`
-    );
-    const data = await response.json();
-    return data;
+    try{
+        const env = getEnviroments();
+        const response = await fetch(
+            `${env.VITE_POKEAPI_HOST}/pokemon-species/${specie}`
+        );
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        return error
+    }
 }
