904883113a49e4e9fd13c72f75688522daa57056
luisrodri45 <luisedurodriguez2014@gmail.com>
2023-01-19 00:02:32 -0300

    chore: add signUpWithGoogle and uid

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,13 +1,16 @@
 import { useReducer } from "react";
 import { getUserLogged, loginUser } from "../../api/backend/user";
-import { types } from "../types/types";
+import { getCurrentUserFirebase } from "../../firebase/getCurrentUserFirebase";
+import { signInWithGoogle } from "../../firebase/providers";
 import { AuthContext, authReducer } from "./";
+import { types } from "../types/types";
 
 const init = () => {
     const token = JSON.parse(localStorage.getItem('token'));
+    const uid = JSON.parse(localStorage.getItem('uid'));
 
     return {
-        logged: !!token,
+        logged: !!token || !!uid,
     }
 }
 
@@ -22,18 +25,20 @@ export const AuthProvider = ({ children }) => {
             payload: true
         }
 
-        const { token , message, type} = await loginUser(username, password);
+        const { token, message, type } = await loginUser(username, password);
 
-        if(token) localStorage.setItem('token', JSON.stringify(token));
+        if (token) {
+            localStorage.setItem('token', JSON.stringify(token));
+            dispatch(action);
+        }
 
-        dispatch(action);
-
-        if(message) return { type, message};
+        if (message) return { type, message };
         return {};
     }
 
     const logout = () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('uid');
         const action = {
             type: types.logout,
         };
@@ -44,18 +49,41 @@ export const AuthProvider = ({ children }) => {
         if (!authState.logged) return undefined
 
         const token = JSON.parse(localStorage.getItem('token'));
+        const uid = JSON.parse(localStorage.getItem('uid'));
+
+        if (uid) {
+            return await getCurrentUserFirebase();
+        };
 
         return await getUserLogged(token);
     }
 
-    const getToken = () =>{
+    const getToken = () => {
         if (!authState.logged) return undefined
 
         return JSON.parse(localStorage.getItem('token'));
     }
 
+    const signUpWithGoogle = async () => {
+        const { ok, data, error } = await signInWithGoogle();
+
+        const action = {
+            type: types.login,
+            payload: true
+        }
+
+        if (ok) {
+            localStorage.setItem('uid', JSON.stringify(data.uid));
+            dispatch(action);
+        }else{
+            return {error};
+        }
+
+        return {};
+    }
+
     return (
-        <AuthContext.Provider value={{ ...authState, login, logout, getUser, getToken }}>
+        <AuthContext.Provider value={{ ...authState, login, logout, getUser, getToken, signUpWithGoogle }}>
             {children}
         </AuthContext.Provider>
     )
