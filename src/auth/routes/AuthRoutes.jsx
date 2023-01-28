4c9b408082f2201b38acde283136b25b9c0ed95e
luisrodri45 <luisedurodriguez2014@gmail.com>
2023-01-28 12:37:32 -0300

    chore: add route 404 for auth route

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,20 +1,18 @@
 
 import { Route, Routes } from 'react-router-dom'
+import { Page404 } from '../../ui/pages/Page404'
 import { LoginPage, SingUpPage } from '../pages'
 
 export const AuthRoutes = () => {
     return (
-        <>
+        <Routes>
 
-            <Routes>
+            <Route path='login' element={<LoginPage />} />
 
-                <Route path='login' element={<LoginPage />} />
+            <Route path='register' element={<SingUpPage />} />
 
-                <Route path='register' element={<SingUpPage />} />
+            <Route path='/*' element={<Page404/>}/>
 
-            </Routes>
-
-
-        </>
+        </Routes>
     )
 }
