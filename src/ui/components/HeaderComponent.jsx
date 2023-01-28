6de3672177f75cc7117ecd2f2432487fcee0d470
luisrodri45 <luisedurodriguez2014@gmail.com>
2023-01-28 13:35:18 -0300

    chore: modify NavLink url

--- src/ui/components/HeaderComponent.jsx
+++ src/ui/components/HeaderComponent.jsx
@@ -84,7 +84,7 @@ export const Header = () => {
                                     <li className="nav-item ms-lg-auto">
                                         <NavLink
                                             className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                                            to="/login"
+                                            to="/auth/login"
                                         >
                                         Log in
                                         </NavLink>
@@ -92,7 +92,7 @@ export const Header = () => {
                                     <li className="nav-item">
                                         <NavLink
                                             className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                                            to="/register"
+                                            to="/auth/register"
                                         >
                                         Sign up
                                         </NavLink>
