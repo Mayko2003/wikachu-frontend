f742b7467acf2c497c91d5be7498dfd032227613
Mayko2003 <maykox34f@gmail.com>
2023-01-28 20:15:01 -0300

    chore: add nature, item, move services to exports

--- src/api/backend/index.js
+++ src/api/backend/index.js
@@ -2,3 +2,6 @@ export * from './favourite';
 export * from './pokemon';
 export * from './user';
 export * from './search';
+export * from './nature';
+export * from './item';
+export * from './move';
