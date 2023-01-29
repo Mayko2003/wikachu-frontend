e7ea2c57aa41c10842a2271884657b4dccbd3216
Mayko2003 <maykox34f@gmail.com>
2023-01-29 00:54:51 -0300

    chore: add type validation for load more, send type to hook

--- src/wikachu/components/Item/ItemList.jsx
+++ src/wikachu/components/Item/ItemList.jsx
@@ -3,9 +3,9 @@ import { Loading } from '../../../ui/components'
 import { useGetItemsByIds } from "../../hooks/Item/useGetItemsByIds.JS"
 
 
-export const ItemList = ({ list }) => {
+export const ItemList = ({ list, type }) => {
 
-    const { data, isLoading, handleLoadMore } = useGetItemsByIds(list)
+    const { data, isLoading, handleLoadMore } = useGetItemsByIds(list, type)
 
     return (
         <div className="row mx-auto col-md-8 animate__animated animate__fadeIn">
@@ -18,7 +18,7 @@ export const ItemList = ({ list }) => {
                                 <ItemCard key={item.id} item={item} />
                             ))
                         }
-                        <div className="d-flex justify-content-center mt-4">
+                        <div className={`${type ? 'd-none' : 'd-flex'} justify-content-center mt-4`}>
                             <button className="text-center btn btn-wk-primary text-light" onClick={handleLoadMore}>
                                 Load more
                             </button>
