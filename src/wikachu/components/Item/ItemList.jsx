32bc402d45e6e8c7c7a5ed9f041955526f5f755c
Mayko2003 <maykox34f@gmail.com>
2023-01-28 21:52:07 -0300

    feat: add item list component

---
+++ src/wikachu/components/Item/ItemList.jsx
@@ -0,0 +1,34 @@
+import { ItemCard } from "./"
+import { Loading } from '../../../ui/components'
+import { useGetItemsByIds } from "../../hooks/Item/useGetItemsByIds.JS"
+
+
+export const ItemList = ({ list }) => {
+
+    const { data, isLoading, handleLoadMore } = useGetItemsByIds(list)
+
+    return (
+        <div className="row mx-auto col-md-8 animate__animated animate__fadeIn">
+            {
+                !isLoading ?
+
+                    <div className="row mx-auto">
+                        {
+                            data.map(item => (
+                                <ItemCard key={item.id} item={item} />
+                            ))
+                        }
+                        <div className="d-flex justify-content-center mt-4">
+                            <button className="text-center btn btn-warning text-light" onClick={handleLoadMore}>
+                                Load more
+                            </button>
+                        </div>
+                    </div>
+
+                    :
+                    <Loading />
+            }
+        </div>
+    )
+
+}
